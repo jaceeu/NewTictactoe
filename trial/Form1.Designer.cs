d93d443175e21f12cf83d4255304ecc9b7f32f49
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-06-02 11:08:01 +0800

    Form to TicTacToe

--- trial/Form1.Designer.cs
+++ trial/Form1.Designer.cs
@@ -173,7 +173,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TicTacToe";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
